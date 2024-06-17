e591c48753ec99af8d46a05001bfe4d5b8ab140e
Priyabrata Das <priyobapi@gmail.com>
2024-06-17 13:47:50 +0530

    modified

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,17 +5,18 @@ import { ErrorHandler } from "../utils/utility.js";
 import jwt from 'jsonwebtoken';
 
 const isAuthenticated = (req,res,next)=>{
-    next();
 
     const token = req.cookies[CHATAPP_TOKEN];
 
+    console.log(token);
+    
     if(!token) return next(new ErrorHandler("Please login to access this route",401));
        
 
-    const decodedData=jwt.verify(token,process.env.JWT_SECRET);
+    const decodedData = jwt.verify(token,process.env.JWT_SECRET);
 
 
-    req.user=decodedData._id;
+    req.user = decodedData?._id;
 
     next();
 };
