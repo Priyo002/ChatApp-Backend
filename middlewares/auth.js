a21c62419437329c520cde14f73ebe40291c9d34
Priyabrata Das <priyobapi@gmail.com>
2024-06-17 13:42:48 +0530

    Modified

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,9 +5,11 @@ import { ErrorHandler } from "../utils/utility.js";
 import jwt from 'jsonwebtoken';
 
 const isAuthenticated = (req,res,next)=>{
-    const token=req.cookies[CHATAPP_TOKEN];
-    if(!token) 
-        return next(new ErrorHandler("Please login to access this route",401));
+    next();
+
+    const token = req.cookies[CHATAPP_TOKEN];
+
+    if(!token) return next(new ErrorHandler("Please login to access this route",401));
        
 
     const decodedData=jwt.verify(token,process.env.JWT_SECRET);
@@ -18,10 +20,11 @@ const isAuthenticated = (req,res,next)=>{
     next();
 };
 
-const adminOnly=(req,res,next)=>{
-    const token=req.cookies["chattapp-admin-token"];
-    if(!token) 
-        return next(new ErrorHandler("Only admin can access this route",401));
+const adminOnly = (req,res,next)=>{
+    
+    const token = req.cookies["chattapp-admin-token"];
+
+    if(!token) return next(new ErrorHandler("Only admin can access this route",401));
        
 
     const secretKey=jwt.verify(token,process.env.JWT_SECRET);
