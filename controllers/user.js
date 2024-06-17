36ad42df9f1f602428fe1b5281fa7c49756ca175
Priyabrata Das <priyobapi@gmail.com>
2024-06-17 13:34:32 +0530

    Modified

--- controllers/user.js
+++ controllers/user.js
@@ -52,10 +52,10 @@ const login =TryCatch(async (req,res,next) => {
 });
 
 const getMyProfile=TryCatch(async(req,res,next)=>{
-    const user=await User.findById(req.user);
+    const user = await User.findById(req.user);
 
     if(!user) return next(new ErrorHandler("User not found",404));
-    res.status(200).json({
+    return res.status(200).json({
         success:true,
         user,
     });
